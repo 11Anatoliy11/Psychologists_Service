cafc3e2a1cace4254a7834a1d170e6fa62004773
11Anatoliy11 <Anatoliy23@i.ua>
2023-11-10 22:06:31 +0200

    Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from "./pages/HomePage/HomePage";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <Router>
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
+      </Router>
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +35,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ src/pages/PsychologistsPage/PsychologistsPage.jsx
@@ -0,0 +1,10 @@
+import PsychologistsList from "../../components/PsychologistsList/PsychologistsList";
+
+const ListPage = () => {
+  return (
+    <>
+      <PsychologistsList />
+    </>
+  );
+};
+export default ListPage;
